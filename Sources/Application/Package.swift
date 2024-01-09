fc46f1088261a73ab3f6adb8775d015e379f71c1
vincentastolfi <vincent.astolfi@etu.uca.fr>
2024-01-09 18:11:10 +0100

    :sparkles: create CommandLineTool package and display basic board

---
+++ Sources/Application/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Application",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(path: "../Model"),
+        .package(path: "../ModelExtensions")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "Application",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Model", package: "Model"),
+                .product(name: "ModelExtensions", package: "ModelExtensions")
+            ]
+        ),
+    ]
+)
